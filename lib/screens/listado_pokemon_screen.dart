71b67f23c47967e54b97450d30b0bc0274fa9e26
Santiago Mangas <santimangas16@gmail.com>
2024-11-22 16:29:40 -0300

    Add theme dark/light in details and form view #2

--- lib/screens/listado_pokemon_screen.dart
+++ lib/screens/listado_pokemon_screen.dart
@@ -1,40 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_guide_2024/mocks/pokemon_mock.dart';
 import 'package:flutter_guide_2024/mocks/pokemon_mock_data.dart';
+import 'package:flutter_guide_2024/utils/pokemon_colors_utils.dart';
 
 class ListadoScreen extends StatelessWidget {
-  // Mapa que asocia tipos de Pokémon con colores
-  final Map<String, Color> typeColors = {
-    'fire': const Color.fromARGB(167, 248, 84, 73),
-    'water': const Color.fromARGB(126, 106, 184, 248),
-    'grass': const Color.fromARGB(155, 108, 196, 49),
-    'electric': Colors.yellow,
-    'bug': Colors.greenAccent,
-    'normal': Colors.brown,
-    'fighting': Colors.purple,
-    'psychic': Colors.pink,
-    'ghost': Colors.deepPurple,
-    'dragon': Colors.deepOrange,
-    // Puedes agregar más tipos si es necesario
-  };
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lista de Pokémon'),
+        title: Text('Lista de Pokemon'),
       ),
       body: ListView.builder(
         itemCount: pokemonMockData.length,
         itemBuilder: (context, index) {
           final Pokemon pokemon = pokemonMockData[index];
-
-          // Obtener el color basado en el primer tipo del Pokémon
-          Color backgroundColor = typeColors[pokemon.types.first.type.name.toLowerCase()] ?? Colors.grey;
+          Color backgroundColor = PokemonColors.getColorForType(pokemon.types.first.type.name); 
 
           return Card(
             margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-            color: backgroundColor, // Aplicamos el color de fondo según el tipo
+            color: backgroundColor, 
             child: ListTile(
               leading: Image.network(
                 pokemon.imageUrl,
